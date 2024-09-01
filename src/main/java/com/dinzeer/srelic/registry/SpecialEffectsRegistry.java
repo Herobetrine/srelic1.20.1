a8fe27b56187d536d5e4d0837c26f850c365ed64
ekk <3085214038@qq.com>
2024-09-01 17:23:49 +0800

    这下是会炸的se了

--- src/main/java/com/dinzeer/srelic/registry/SpecialEffectsRegistry.java
+++ src/main/java/com/dinzeer/srelic/registry/SpecialEffectsRegistry.java
@@ -4,11 +4,9 @@ import java.util.function.Supplier;
 
 
 import com.dinzeer.srelic.Srelic;
-import com.dinzeer.srelic.registry.SpecialEffectsRegistry;
-import com.dinzeer.srelic.registry.specialeffects.SpecialEffect;
 import com.dinzeer.srelic.registry.specialeffects.VeryGreen;
 
-import mods.flammpfeil.slashblade.SlashBlade;
+import mods.flammpfeil.slashblade.registry.specialeffects.SpecialEffect;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,10 +17,9 @@ import net.minecraftforge.registries.RegistryObject;
 import static com.dinzeer.srelic.Srelic.*;
 
 public class SpecialEffectsRegistry {
-    public static final ResourceKey<Registry<SpecialEffect>> REGISTRY_KEY2 = ResourceKey
-            .createRegistryKey(Srelic.prefix("special_effect"));
-    public static final DeferredRegister<SpecialEffect> SPECIAL_EFFECT =
-            DeferredRegister.create(REGISTRY_KEY2, MODID);
+
+    public static final DeferredRegister<SpecialEffect> SPECIAL_EFFECT = DeferredRegister.create(SpecialEffect.REGISTRY_KEY,
+            MODID);
 
     public static final Supplier<IForgeRegistry<SpecialEffect>> REGISTRY = SPECIAL_EFFECT.makeRegistry(RegistryBuilder::new);
 
--- src/main/java/com/dinzeer/srelic/registry/specialeffects/VeryGreen.java
+++ src/main/java/com/dinzeer/srelic/registry/specialeffects/VeryGreen.java
@@ -1,13 +1,6 @@
 package com.dinzeer.srelic.registry.specialeffects;
 
-import mods.flammpfeil.slashblade.capability.slashblade.ISlashBladeState;
-import mods.flammpfeil.slashblade.event.SlashBladeEvent;
-import mods.flammpfeil.slashblade.registry.SpecialEffectsRegistry;
-
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import mods.flammpfeil.slashblade.registry.specialeffects.SpecialEffect;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber
 public class VeryGreen extends SpecialEffect {
